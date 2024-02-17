4461473e881e49c2798f81434e38d5ec41d106ac
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-02-17 23:49:09 +0300

    Authorization enhanced

---
+++ src/Authorization/Roles/BlogEditorRole.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace NextDeveloper\Blogs\Authorization\Roles;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use NextDeveloper\CRM\Database\Models\AccountManagers;
+use NextDeveloper\IAM\Authorization\Roles\AbstractRole;
+use NextDeveloper\IAM\Authorization\Roles\IAuthorizationRole;
+use NextDeveloper\IAM\Database\Models\Users;
+use NextDeveloper\IAM\Helpers\UserHelper;
+
+class BlogEditorRole extends AbstractRole implements IAuthorizationRole
+{
+    public const NAME = 'blog-editor';
+
+    public const LEVEL = 40;
+
+    public const DESCRIPTION = 'Blog Editor';
+
+    public const DB_PREFIX = 'blog';
+
+    /**
+     * Applies basic member role sql for Eloquent
+     *
+     * @param Builder $builder
+     * @param Model $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        /**
+         * Here user will be able to list all models, because by default, sales manager can see everybody.
+         */
+        $ids = AccountManagers::withoutGlobalScopes()
+            ->where('iam_account_id', UserHelper::currentAccount()->id)
+            ->pluck('crm_account_id');
+
+        $builder->whereIn('iam_account_id', $ids);
+    }
+
+    public function checkPrivileges(Users $users = null)
+    {
+        //return UserHelper::hasRole(self::NAME, $users);
+    }
+
+    public function getModule()
+    {
+        return 'blog';
+    }
+
+    public function allowedOperations() :array
+    {
+        return [
+            'blog_posts:read',
+            'blog_posts:update',
+            'blog_posts:create',
+            'blog_posts:delete',
+        ];
+    }
+
+    public function getLevel(): int
+    {
+        return self::LEVEL;
+    }
+
+    public function getDescription(): string
+    {
+        return self::DESCRIPTION;
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function canBeApplied($column)
+    {
+        if(self::DB_PREFIX === '*') {
+            return true;
+        }
+
+        if(Str::startsWith($column, self::DB_PREFIX)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getDbPrefix()
+    {
+        return self::DB_PREFIX;
+    }
+}
