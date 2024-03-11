8e64c514bfb70d7bfafe803f9f710dcbc1f8977c
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-03-12 00:31:35 +0300

    Blog reader role added

---
+++ src/Authorization/Roles/BlogReaderRole.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace NextDeveloper\Blogs\Authorization\Roles;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use NextDeveloper\CRM\Database\Models\AccountManagers;
+use NextDeveloper\IAM\Authorization\Roles\AbstractRole;
+use NextDeveloper\IAM\Authorization\Roles\IAuthorizationRole;
+use NextDeveloper\IAM\Database\Models\Users;
+use NextDeveloper\IAM\Helpers\UserHelper;
+
+class BlogReaderRole extends AbstractRole implements IAuthorizationRole
+{
+    public const NAME = 'blog-reader';
+
+    public const LEVEL = 10;
+
+    public const DESCRIPTION = 'Blog Reader';
+
+    public const DB_PREFIX = 'blog';
+
+    /**
+     * Applies basic member role sql for Eloquent
+     *
+     * @param Builder $builder
+     * @param Model $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        /**
+         * Readers can read any public blog post
+         */
+        $builder->where('is_draft', 'false');
+        $builder->where('is_active', 'true');
+    }
+
+    public function checkPrivileges(Users $users = null)
+    {
+        //return UserHelper::hasRole(self::NAME, $users);
+    }
+
+    public function getModule()
+    {
+        return 'blogs';
+    }
+
+    public function allowedOperations() :array
+    {
+        return [
+            'blog_posts:read'
+        ];
+    }
+
+    public function getLevel(): int
+    {
+        return self::LEVEL;
+    }
+
+    public function getDescription(): string
+    {
+        return self::DESCRIPTION;
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function canBeApplied($column)
+    {
+        if(self::DB_PREFIX === '*') {
+            return true;
+        }
+
+        if(Str::startsWith($column, self::DB_PREFIX)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getDbPrefix()
+    {
+        return self::DB_PREFIX;
+    }
+}
